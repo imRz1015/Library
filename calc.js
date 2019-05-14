c9a56c4ede9fe7b3a67245dc63781297d13dc215
TANGiMING <tangiming9510@163.com>
2019-05-14 20:39:32 +0800

    解决加减乘除丢失精度的问题

---
+++ calc.js
@@ -0,0 +1,74 @@
+/**
+ * created by TANGIMING
+ *
+ * 2019-3-30
+ *
+ * 这是处理JS加减乘除丢失精度的方法
+ * **/
+
+export default {
+    //除法
+    $div(arg1, arg2) {
+        if (!arg1) return 0;
+        let t1 = 0, t2 = 0, r1, r2;
+        try {
+            t1 = arg1.toString().split(".")[1].length
+        } catch (e) {
+        }
+        try {
+            t2 = arg2.toString().split(".")[1].length
+        } catch (e) {
+        }
+        r1 = Number(arg1.toString().replace(".", ""));
+        r2 = Number(arg2.toString().replace(".", ""));
+        return this.accMul((r1 / r2), Math.pow(10, t2 - t1));
+    },
+    //乘法
+    $mul(arg1, arg2) {
+        if (!arg1) return 0;
+        let m = 0, s1 = arg1.toString(), s2 = arg2.toString();
+        try {
+            m += s1.split(".")[1].length
+        } catch (e) {
+        }
+        try {
+            m += s2.split(".")[1].length
+        } catch (e) {
+        }
+        return Number(s1.replace(".", "")) * Number(s2.replace(".", "")) / Math.pow(10, m)
+    },
+
+    //加法
+    $plus(arg1, arg2) {
+        let r1, r2, m;
+        try {
+            r1 = arg1.toString().split(".")[1].length
+        } catch (e) {
+            r1 = 0
+        }
+        try {
+            r2 = arg2.toString().split(".")[1].length
+        } catch (e) {
+            r2 = 0
+        }
+        m = Math.pow(10, Math.max(r1, r2))
+        return (arg1 * m + arg2 * m) / m
+    },
+    //减法
+    $sub(arg1, arg2) {
+        let r1, r2, m, n;
+        try {
+            r1 = arg1.toString().split(".")[1].length
+        } catch (e) {
+            r1 = 0
+        }
+        try {
+            r2 = arg2.toString().split(".")[1].length
+        } catch (e) {
+            r2 = 0
+        }
+        m = Math.pow(10, Math.max(r1, r2));
+        n = (r1 >= r2) ? r1 : r2;
+        return ((arg1 * m - arg2 * m) / m).toFixed(n);
+    }
+}
